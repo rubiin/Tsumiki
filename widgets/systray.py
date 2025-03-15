26ed7d4ba1ce6c6242578121d15508f289aad167
PixelKhaos <pixelkhaos@gmail.com>
2025-03-15 11:18:24 +0100

    refactor: systray 'hidden' config & improved styling

--- widgets/systray.py
+++ widgets/systray.py
@@ -5,6 +5,7 @@ from fabric.widgets.image import Image
 from gi.repository import Gdk, GdkPixbuf, GLib, Gray, Gtk
 
 from shared.pop_over import PopOverWindow
+from shared.separator import Separator
 from shared.widget_container import BoxWidget, ButtonWidget
 from utils.widget_settings import BarConfig
 
@@ -117,11 +118,11 @@ class SystemTrayWidget(ButtonWidget):
         self.tray_box = Box(
             name="system-tray-box",
             orientation=Gtk.Orientation.HORIZONTAL,
-            spacing=4
+            spacing=8
         )
         self.toggle_icon = Image(
             icon_name="arrow-down-symbolic",
-            icon_size=16,
+            icon_size=self.config["icon_size"],
             style_classes=["panel-icon", "toggle-icon"]
         )
 
@@ -130,10 +131,10 @@ class SystemTrayWidget(ButtonWidget):
         self.children = Box(
             spacing=8,
             orientation=Gtk.Orientation.HORIZONTAL,
-            children=(self.tray_box, self.toggle_icon)
+            children=(self.tray_box, Separator(), self.toggle_icon)
         )
 
-        # Create popup menu for ignored items
+        # Create popup menu for hidden items
         self.popup_menu = SystemTrayMenu(config=self.config)
         self.popup = PopOverWindow(
             name="popup",
@@ -161,11 +162,17 @@ class SystemTrayWidget(ButtonWidget):
         visible = self.popup.get_visible()
         if visible:
             self.popup.hide()
-            self.toggle_icon.set_from_icon_name("arrow-down-symbolic", 16)
+            self.toggle_icon.set_from_icon_name(
+                "arrow-down-symbolic",
+            self.config["icon_size"]
+            )
             self.toggle_icon.get_style_context().remove_class("active")
         else:
             self.popup.show_all()
-            self.toggle_icon.set_from_icon_name("arrow-up-symbolic", 16)
+            self.toggle_icon.set_from_icon_name(
+                "arrow-up-symbolic",
+                self.config["icon_size"]
+            )
             self.toggle_icon.get_style_context().add_class("active")
 
     def on_item_added(self, _, identifier: str):
@@ -176,12 +183,12 @@ class SystemTrayWidget(ButtonWidget):
         # Get item title for matching
         title = item.get_property("title") or ""
 
-        # Check if item should be ignored based on title
-        ignored_list = self.config.get("ignored", [])
-        is_ignored = any(x.lower() in title.lower() for x in ignored_list)
+        # Check if item should be hidden based on title
+        hidden_list = self.config.get("hidden", [])
+        is_hidden = any(x.lower() in title.lower() for x in hidden_list)
 
         # Add to appropriate container
-        if is_ignored:
+        if is_hidden:
             self.popup_menu.add_item(item)
         else:
             button = Button(style_classes="flat")
@@ -191,6 +198,8 @@ class SystemTrayWidget(ButtonWidget):
                 event: self.popup_menu.on_button_click(button, item, event),
             )
             button.set_tooltip_text(title)
+            button.set_margin_start(2)
+            button.set_margin_end(2)
 
             # Update icon
             pixmap = Gray.get_pixmap_for_pixmaps(item.get_icon_pixmaps(), 24)
