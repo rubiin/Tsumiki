182cfc22adba4ecfbfd19a2e36519c316fe36967
Rubin Bhandari <roobin.bhandari@gmail.com>
2025-11-22 00:32:31 +0545

    style: improve table CSS formatting and responsiveness
    - Adjusted padding and margin for table header and cells.
    - Enhanced light and dark theme styles for table headers.
    - Improved responsive design for smaller screens.
    - Fixed formatting in tsconfig.json for consistency.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,59 +1,59 @@
 // @ts-check
-import starlight from '@astrojs/starlight';
-import { defineConfig } from 'astro/config';
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: 'Tsumiki',
-      customCss: [
-        // Path to our custom CSS file
-        './src/styles/custom.css',
-      ],
-      components: {
-        LanguageSelect: './src/components/LanguageSelect.astro',
-      },
-      defaultLocale: 'en',
-      locales: {
-        en: { label: 'English', lang: 'en' },
-        es: { label: 'Español', lang: 'es' }
-      },
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/rubiin/Tsumiki',
-        },
-        {
-          icon: 'discord',
-          label: 'Discord',
-          href: 'https://discord.gg/8nWbDC4SnP',
-        },
-      ],
-      sidebar: [
-        {
-          label: '🚀 Getting Started',
-          autogenerate: {directory: 'getting-started'},
-        },
-        {
-          label: '🛠️ Configuring',
-          autogenerate: {directory: 'configuring'},
-        },
+	integrations: [
+		starlight({
+			title: "Tsumiki",
+			customCss: [
+				// Path to our custom CSS file
+				"./src/styles/custom.css",
+			],
+			components: {
+				LanguageSelect: "./src/components/LanguageSelect.astro",
+			},
+			defaultLocale: "en",
+			locales: {
+				en: { label: "English", lang: "en" },
+				es: { label: "Español", lang: "es" },
+			},
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/rubiin/Tsumiki",
+				},
+				{
+					icon: "discord",
+					label: "Discord",
+					href: "https://discord.gg/8nWbDC4SnP",
+				},
+			],
+			sidebar: [
+				{
+					label: "🚀 Getting Started",
+					autogenerate: { directory: "getting-started" },
+				},
+				{
+					label: "🛠️ Configuring",
+					autogenerate: { directory: "configuring" },
+				},
 
-        {
-          label: '🎨 Theming',
-          autogenerate: {directory: 'theming'},
-        },
-        {
-          label: '📚 Resources',
-          autogenerate: {directory: 'resources'}
-        },
-        {
-          label: '👥 Help',
-          autogenerate: {directory: 'help'}
-        }
-      ],
-    }),
-  ],
+				{
+					label: "🎨 Theming",
+					autogenerate: { directory: "theming" },
+				},
+				{
+					label: "📚 Resources",
+					autogenerate: { directory: "resources" },
+				},
+				{
+					label: "👥 Help",
+					autogenerate: { directory: "help" },
+				},
+			],
+		}),
+	],
 });
