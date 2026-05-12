169f2cf8f62f3d10414f9328e2d8626618884219
Rubin Bhandari <roobin.bhandari@gmail.com>
2026-05-12 14:07:32 +0545

    Add pnpm workspace configuration and theme settings
    - Created a new pnpm-workspace.yaml file to allow builds for esbuild and sharp.
    - Added a comprehensive theme.toml file with various styling options for the bar and its widgets, including borders, margins, and icon sizes.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -2,6 +2,7 @@
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
 
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://tsumikii.pages.dev",
